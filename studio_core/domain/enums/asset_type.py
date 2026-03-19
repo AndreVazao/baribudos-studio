f6543869410547812ad35c2c6e111b330cc2e1e4
AndreVazao <vazao.andre@gmail.com>
2026-03-19 13:06:07 +0000

    Create asset_type.py

---
+++ studio_core/domain/enums/asset_type.py
@@ -0,0 +1,16 @@
+ASSET_TYPES = {
+    "studio_logo",
+    "ip_logo",
+    "cover",
+    "hero_background",
+    "gallery_image",
+    "thumbnail",
+    "ornament",
+    "badge",
+    "promo_banner",
+    "character_card",
+    "background_texture",
+    "trailer_thumbnail",
+    "social_card",
+    "campaign_visual"
+}
