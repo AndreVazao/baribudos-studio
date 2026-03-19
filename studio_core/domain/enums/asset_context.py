ef61460bc62751e5cfda739cb04b31fe965bc957
AndreVazao <vazao.andre@gmail.com>
2026-03-19 13:06:51 +0000

    Create asset_context.py

---
+++ studio_core/domain/enums/asset_context.py
@@ -0,0 +1,16 @@
+ASSET_CONTEXTS = {
+    "global_header",
+    "global_footer",
+    "homepage",
+    "ip_page",
+    "product_page",
+    "checkout",
+    "library",
+    "admin",
+    "campaign",
+    "marketing_landing",
+    "email",
+    "social",
+    "marketplace_amazon",
+    "marketplace_website"
+}
