e23f3ef49624bcf2b0178dc216c207da9aca9d65
AndreVazao <vazao.andre@gmail.com>
2026-03-21 20:11:57 +0000

    Update main.py

--- studio_core/api/main.py
+++ studio_core/api/main.py
@@ -57,7 +57,8 @@ from studio_core.api.routes.users import ensure_default_owner, router as users_r
 from studio_core.api.routes.v1_readiness import router as v1_readiness_router
 from studio_core.api.routes.videos import router as videos_router
 from studio_core.api.routes.voice_library import router as voice_library_router
-from studio_core.api.routes.assets import router as assets_router
+from studio_core.api.routes.assets import router as assets_routerfromroms
+from studio_core.api.routes.storefront import router as storefront_router
 from studio_core.api.routes.voice_preview import router as voice_preview_router
 from studio_core.core.config import APP_CONFIG, resolve_project_path, resolve_storage_path
 from studio_core.services.ai_runtime_bootstrap import start_all
@@ -146,9 +147,8 @@ app.include_router(ip_characters_router, prefix="/api")
 app.include_router(ip_canons_router, prefix="/api")
 app.include_router(ip_metadata_router, prefix="/api")
 app.include_router(illustrations_router, prefix="/api")
-app.include_router(assets_router, prefix="/api")
-
-# Public media contract for Website consumption
+app.include_router(assets_router, prefix="/apiPublicPublic media contract for Website consumption
+app.include_router(storefront_router)
 app.include_router(public_assets_router, prefix="/api")
 
 
