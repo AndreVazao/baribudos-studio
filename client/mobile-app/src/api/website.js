844ffca6b8fa563ae6bf844dffe8f12d21d0e6fd
AndreVazao <vazao.andre@gmail.com>
2026-03-26 09:24:58 +0000

    feat: add Studio frontend Website control api

---
+++ client/mobile-app/src/api/website.js
@@ -0,0 +1,21 @@
+import { getApiBase, get, post, qs } from "./core.js"
+
+export async function getWebsiteControlSummary() {
+  return get(`${getApiBase()}/website-control/summary`)
+}
+
+export async function getWebsiteControlCatalog({ limit = 25, activeOnly = false } = {}) {
+  return get(`${getApiBase()}/website-control/catalog${qs({ limit, active_only: activeOnly })}`)
+}
+
+export async function getWebsitePublishEnvelope(projectId) {
+  return get(`${getApiBase()}/website-publisher/envelope/${projectId}`)
+}
+
+export async function getWebsitePublishStatus(projectId) {
+  return get(`${getApiBase()}/website-publisher/status/${projectId}`)
+}
+
+export async function publishProjectToWebsite(projectId) {
+  return post(`${getApiBase()}/website-publisher/publish/${projectId}`, {})
+}
